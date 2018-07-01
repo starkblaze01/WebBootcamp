be91bb5323ab0b0a2bababd3e19ed4546dc1dbf4
Mayank Pathela <mp.pathela@gmail.com>
2018-07-01 20:58:31 +0530

    Add JS_Basic files

---
+++ JS_Basics/Demo.js
@@ -0,0 +1,6 @@
+var firstName = prompt("What is your First Name?");
+var lastName = prompt("What is your Last Name?");
+var yourAge = prompt("What is your Age?");
+alert("Nice to meet you!, "+firstName+" "+lastName);
+console.log("You full name is "+firstName+" "+lastName);
+console.log("Your are "+yourAge+ " years old");
