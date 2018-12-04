7889f657e23c3fecc4304e4b40d21b081930aab6
starkblaze01 <mp.pathela@gmail.com>
2018-12-04 14:36:17 -0800

    Add Loop Exercise

---
+++ JS_Basics/loopexecise.js
@@ -0,0 +1,40 @@
+console.log("Print all numbers between -10 and 19");
+
+var count=-9;
+
+while(count<19){
+    console.log(count);
+    count++;
+}
+
+console.log("Print all even numbers between 10 and 40");
+
+count = 11;
+while(count<40)
+{   if(count%2===0)
+    {
+        console.log(count);
+    }
+    count++;
+}
+
+console.log("Print all odd numbers between 300 and 333");
+
+count= 301;
+while(count<333)
+{   if(count%2!==0)
+    {
+        console.log(count);
+    }
+    count++;
+}
+
+console.log("Print all numbers divisible by 5 AND 3 between 5 and 50");
+
+count=5;
+while(count<50){
+    if(count%5===0 && count%3===0){
+        console.log(count);
+    }
+    count++;
+}
